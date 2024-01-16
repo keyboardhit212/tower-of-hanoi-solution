67947f621a31ae2d67c23c651d8c0a93b5910aee
keyboardhit212 <rjudd.celis@gmail.com>
2024-01-16 14:44:31 +0800

    Added move count output

--- Main.java
+++ Main.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static long count = 0;
+
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.print("Enter number of discs: ");
         int disc = console.nextInt();
         towerOfHanoi(disc, 'A', 'B', 'C');
+        System.out.println(String.format("You made %d moves", count));
     }
 
     public static void towerOfHanoi(int discNumber, char start, char aux, char end) {
+        count++;
         if (discNumber == 1)
             System.out.println(String.format("Move disc %d from %c with %c as aux going to %c", discNumber, start, aux, end));
         else {
